4a061f161776404bbbb48db53b64cbe07e70e614
Muhammad Rendra <muhammadnurrendra@gmail.com>
2023-01-22 13:32:24 +0700

    Update `@gh/ghVersion` • add `src/gh/ghVersion/types` • add `verbose` option

--- src/gh/ghVersion/ghVersion.ts
+++ src/gh/ghVersion/ghVersion.ts
@@ -1,14 +1,23 @@
 import { exec } from '@cp'
 import log, { c } from '@clog'
+import { Options } from './types'
 
-const ghVersion = async () => {
-  const { stdall } = await exec('gh --version')
+const ghVersion = async ({
+  verbose
+}: Options = {}) => {
+  const cmd = 'gh --version'
+
+  verbose && log(c.blue(`• ${cmd}`))
+
+  const { stdall } = await exec(cmd)
 
   if (stdall.includes('command not found')) {
     log(c.red(`You don't have ${c.bold('GitHub CLI')}.`))
     log(c.red(`Please follow ${c.underline('https://cli.github.com/')} to install ${c.bold('gh')} command line.`))
     process.exit()
   }
+
+  verbose && log(c.grey(stdall))
 
   return stdall
 }
