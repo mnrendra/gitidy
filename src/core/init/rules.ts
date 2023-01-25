3a1e35bc8ca930562c6856dd2cf35db194d97ad6
Muhammad Rendra <mnrendra@users.noreply.github.com>
2023-01-25 22:34:19 +0700

    Add new protection rules (#56)
    • add protection rule for `release` branch
    • add protection rule for `hotfix` branches

--- src/core/init/rules.ts
+++ src/core/init/rules.ts
@@ -70,7 +70,79 @@ export const dev: Rule = {
   enforce_admins: false,
 }
 
+export const release: Rule = {
+  required_pull_request_reviews: {
+    required_approving_review_count: 2,
+    dismiss_stale_reviews: true,
+    require_code_owner_reviews: true,
+    require_last_push_approval: true,
+    // dismissal_restrictions: {
+    //   users: [],
+    //   teams: [],
+    // },
+    // bypass_pull_request_allowances: {
+    //   users: [],
+    //   teams: [],
+    // }
+  },
+  required_status_checks: {
+    strict: true,
+    contexts: [],
+  },
+  required_conversation_resolution: true,
+  required_linear_history: true,
+  lock_branch: false,
+  allow_force_pushes: false,
+  allow_deletions: true,
+  restrictions: null,
+  // restrictions: {
+  //   users: [],
+  //   teams: [],
+  //   apps: []
+  // },
+  block_creations: false,
+  allow_fork_syncing: false,
+  enforce_admins: false,
+}
+
+export const hotfix: Rule = {
+  required_pull_request_reviews: {
+    required_approving_review_count: 2,
+    dismiss_stale_reviews: true,
+    require_code_owner_reviews: true,
+    require_last_push_approval: true,
+    // dismissal_restrictions: {
+    //   users: [],
+    //   teams: [],
+    // },
+    // bypass_pull_request_allowances: {
+    //   users: [],
+    //   teams: [],
+    // }
+  },
+  required_status_checks: {
+    strict: true,
+    contexts: [],
+  },
+  required_conversation_resolution: true,
+  required_linear_history: true,
+  lock_branch: false,
+  allow_force_pushes: false,
+  allow_deletions: true,
+  restrictions: null,
+  // restrictions: {
+  //   users: [],
+  //   teams: [],
+  //   apps: []
+  // },
+  block_creations: false,
+  allow_fork_syncing: false,
+  enforce_admins: false,
+}
+
 export default {
   main,
-  dev
+  dev,
+  release,
+  hotfix
 }
